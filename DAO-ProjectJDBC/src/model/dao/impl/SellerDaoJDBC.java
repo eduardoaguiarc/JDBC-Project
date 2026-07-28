4e2f9f351a92f9f2a4518dd460bbb89ca585a6a1
Eduardo Aguiar <eduardocamposaguiar57@gmail.com>
2026-07-28 20:58:53 -0300

    DeleteById

--- DAO-ProjectJDBC/src/model/dao/impl/SellerDaoJDBC.java
+++ DAO-ProjectJDBC/src/model/dao/impl/SellerDaoJDBC.java
@@ -57,32 +57,45 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-public void update(Seller obj) {
-    PreparedStatement st = null;
-    try {
-        st = conn.prepareStatement(
-                "UPDATE seller SET name = ?, email = ?, birthdate = ?, basesalary = ?, departmentid = ? WHERE id = ?");
+    public void update(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE seller SET name = ?, email = ?, birthdate = ?, basesalary = ?, departmentid = ? WHERE id = ?");
 
-        st.setString(1, obj.getName());
-        st.setString(2, obj.getEmail());
-        st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
-        st.setDouble(4, obj.getBaseSalary());
-        st.setInt(5, obj.getDepartment().getId());
-        st.setInt(6, obj.getId());
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
 
-        st.executeUpdate();
+            st.executeUpdate();
 
-    } catch (SQLException e) {
-        throw new DbException(e.getMessage());
-    } finally {
-        DB.closeStatement(st);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
-}
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+            st.executeUpdate();
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DbException("No seller found with the provided ID.");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
